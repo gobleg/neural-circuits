32278428c5dd3b9cab679b793b370f1511113df5
swampfox76 <swamp.fox076@gmail.com>
2017-12-02 15:18:32 -0600

    Added nn to graph

--- graph.py
+++ graph.py
@@ -5,9 +5,16 @@ import matplotlib.pyplot as plt
 if len(sys.argv) != 2:
     print "Please specify a circuit type"
 circuit = sys.argv[1]
+
 npzfile = np.load('results.npz')
 names = npzfile['arr_0']
 values = npzfile['arr_1']
+npzfile2 = np.load('results_nn.npz')
+name = npzfile2['arr_0']
+value = npzfile2['arr_1']
+names = np.append(names, name)
+values = np.append(values, value)
+
 y_pos = np.arange(len(names))
 plt.barh(y_pos, values, align='center')
 plt.yticks(y_pos, names)
